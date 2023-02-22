2ceddaa18251c8d92aee5d81a1ba986434938744
hezhongfeng <287645587@qq.com>
2023-02-22 14:27:40 +0800

    :sparkles: feat(custom): 添加图片资源

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,11 @@
 <template>
-  <header>header</header>
-
-  <main>welcome</main>
+  <div class="app">App</div>
 </template>
 
 <script setup></script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style lang="scss">
+.app {
+  min-height: 100vh;
 }
 </style>
