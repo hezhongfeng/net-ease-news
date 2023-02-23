9cf26b2fff111b785b7beb56ff15cf96dcb44283
hezhongfeng <287645587@qq.com>
2023-02-23 10:19:41 +0800

    :lipstick: style(custom): 绘画出第一张图

--- src/components/PictureInPicture/PictureInPicture.vue
+++ src/components/PictureInPicture/PictureInPicture.vue
@@ -1,30 +1,64 @@
 <template>
   <div class="picture-in-picture">
-    <canvas ref="canvas"> 画中画 </canvas>
+    <canvas ref="canvas" :width="canvasWidth" :height="canvasHeight">
+      画中画
+    </canvas>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import Picture from "../../assets/images/cover_v2.jpg";
 
-// const props = defineProps();
 const canvas = ref();
 
 // canvas的ctx
 let ctx = null;
 
-onMounted(() => {
-  ctx = canvas.value.getContext("2d");
+const img = new Image();
 
-  console.log(start);
+onMounted(() => {
+  img.src = Picture;
+  img.onload = () => {
+    start();
+    console.log("img.onload");
+  };
+  ctx = canvas.value.getContext("2d");
 });
 
 const start = () => {
   console.log(ctx);
+
+  step();
+};
+
+const step = () => {
+  draw();
+  // window.requestAnimationFrame(step);
+};
+
+const canvasWidth = document.documentElement.clientWidth;
+
+// 按照图片的长宽比，计算出canvas的高度，注意不能使用样式去控制canvas
+const canvasHeight = canvasWidth / 0.6218;
+
+const draw = () => {
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    0,
+    0,
+    canvasWidth,
+    canvasHeight
+  );
 };
 </script>
 
 <style lang="scss">
 .picture-in-picture {
+  width: 100%;
 }
 </style>
