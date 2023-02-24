52e75f464cf5436b5c62ea05d5d5093a90eb8604
hezhongfeng <287645587@qq.com>
2023-02-24 09:36:10 +0800

    :sparkles: feat(custom): 图片的完美缩放

--- src/components/PictureInPicture/PictureInPicture.vue
+++ src/components/PictureInPicture/PictureInPicture.vue
@@ -8,22 +8,57 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import Picture from "../../assets/images/cover_v2.jpg";
+// import Picture from "../../assets/images/cover_v2.jpg";
+import P1 from "../../assets/images/p1.jpg";
+import P2 from "../../assets/images/p2.jpg";
 
 const canvas = ref();
 
 // canvas的ctx
 let ctx = null;
 
-const img = new Image();
+// 图片最初的尺寸
+// const imageOriginalWith = 750;
+// const imageOriginalHeight = 1206;
 
-onMounted(() => {
-  img.src = Picture;
-  img.onload = () => {
-    start();
-    console.log("img.onload");
-  };
+// const imageOriginalWith = 1875;
+// const imageOriginalHeight = 3015;
+
+const ImgSrcs = [P1, P2];
+const images = [];
+
+// let radio = 556 / 1875;
+let radio = 1;
+
+let timer = null;
+
+const loadImages = async () => {
+  const promises = [];
+  for (const src of ImgSrcs) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          resolve();
+        };
+        img.onerror = () => {
+          reject();
+        };
+        images.push(img);
+      })
+    );
+  }
+
+  await Promise.all(promises);
+  console.log("图片加载完毕");
+};
+
+onMounted(async () => {
+  await loadImages();
+
   ctx = canvas.value.getContext("2d");
+  start();
 });
 
 const start = () => {
@@ -33,8 +68,15 @@ const start = () => {
 };
 
 const step = () => {
-  draw();
-  // window.requestAnimationFrame(step);
+  // 缩小后停止;
+  if (radio < 556 / 1875) {
+    console.log(72);
+    window.cancelAnimationFrame(timer);
+  } else {
+    draw();
+    timer = window.requestAnimationFrame(step);
+  }
+  // draw();
 };
 
 const canvasWidth = document.documentElement.clientWidth;
@@ -43,17 +85,55 @@ const canvasWidth = document.documentElement.clientWidth;
 const canvasHeight = canvasWidth / 0.6218;
 
 const draw = () => {
+  console.log("drawing");
+
+  // 由大到小
   ctx.drawImage(
-    img,
-    0,
-    0,
-    img.width,
-    img.height,
+    images[1],
+    1251 * ((1875 - 556 / radio) / (1875 - 556)), // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
+    1050 * ((1875 - 556 / radio) / (1875 - 556)), // EN/IT= AE/AI=AQ/AR
+    556 / radio, //   开始是innerWidth，慢慢变大到imageOriginalWith
+    894 / radio,
     0,
     0,
     canvasWidth,
     canvasHeight
   );
+
+  // width: '556',
+  //       height: '894',
+  //       left: '1251',
+  //       top: '1050',
+
+  // const imageOriginalWith = 1875;
+  // const imageOriginalHeight = 3015;
+
+  // ctx.drawImage(
+  //   images[1],
+  //   0,
+  //   0,
+  //   1875,
+  //   3015,
+  //   0,
+  //   0,
+  //   canvasWidth,
+  //   canvasHeight
+  // );
+
+  // // 由大到小
+  // ctx.drawImage(
+  //   images[0],
+  //   0,
+  //   0,
+  //   imageOriginalWith,
+  //   imageOriginalHeight,
+  //   0,
+  //   0,
+  //   canvasWidth * radio,
+  //   canvasHeight * radio
+  // );
+
+  radio = radio * 0.993;
 };
 </script>
 
