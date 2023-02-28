93001d1e5ed9728b141bbce1cf87cc2e8493a0ca
hezhongfeng <287645587@qq.com>
2023-02-28 14:54:03 +0800

    :zap: perf(custom): 较顺利的完成绘画和场景切换

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <PictureInPicture :images-data="imagesData" />
+    <PictureInPicture :images-data="imagesData" :factor="0.99" />
   </div>
 </template>
 
@@ -10,6 +10,8 @@ import { PictureInPicture } from "./components/PictureInPicture";
 import Cover from "./assets/images/cover_v2.jpg";
 import P1 from "./assets/images/p1.jpg";
 import P2 from "./assets/images/p2.jpg";
+import P3 from "./assets/images/p3.jpg";
+import P4 from "./assets/images/p4.jpg";
 
 const imagesData = [
   {
@@ -37,6 +39,28 @@ const imagesData = [
       height: "894",
       left: "1251",
       top: "1050",
+    },
+  },
+  {
+    src: P3,
+    width: "1875",
+    height: "3015",
+    innerImg: {
+      width: "166",
+      height: "267",
+      left: "114",
+      top: "897",
+    },
+  },
+  {
+    src: P4,
+    width: "1875",
+    height: "3015",
+    innerImg: {
+      width: "194",
+      height: "312",
+      left: "85",
+      top: "1402",
     },
   },
 ];
--- src/components/PictureInPicture/PictureInPicture.vue
+++ src/components/PictureInPicture/PictureInPicture.vue
@@ -10,14 +10,17 @@
 import { ref, onMounted } from "vue";
 
 // eslint-disable-next-line vue/no-setup-props-destructure
-const { imagesData } = defineProps(["imagesData"]);
+const { imagesData, factor } = defineProps(["imagesData", "factor"]);
 
 const canvas = ref();
 
+// 缩放的index
+let playIndex = 0;
+
 let ctx = null;
 
-// 最初的缩小系数
-let factor = 1;
+// 最初的变化
+let reduce = 1;
 
 // requestAnimationFrame的请求ID，可以用来暂停和取消
 let requestId = null;
@@ -45,41 +48,55 @@ const loadImages = async () => {
   console.log("图片加载完毕");
 };
 
-onMounted(async () => {
-  ctx = canvas.value.getContext("2d");
-  await loadImages();
-  // 画布和图片准备好了，开始画图
-  start();
-});
-
+// 设备的宽度
 const canvasWidth = document.documentElement.clientWidth;
 
 // 按照图片的长宽比，计算出canvas的高度，注意不能使用样式去控制canvas
 const canvasHeight = canvasWidth / 0.6218;
 
+let currentImage = null;
+
+let nextImage = null;
+
 const start = () => {
+  // 起始的两张图片
+  currentImage = imagesData[playIndex];
+  nextImage = imagesData[playIndex + 1];
   step();
+};
+
+// 切换下一张
+const nextStep = () => {
+  playIndex += 1;
+  if (playIndex > imagesData.length - 2) {
+    stop();
+  } else {
+    currentImage = imagesData[playIndex];
+    nextImage = imagesData[playIndex + 1];
+    reduce = 1;
+    requestId = window.requestAnimationFrame(step);
+  }
+};
+
+const stop = () => {
+  window.cancelAnimationFrame(requestId);
 };
 
 const step = () => {
   // 缩小到PIP的大小和位置
-  if (factor < 152 / 1875) {
-    window.cancelAnimationFrame(requestId);
+  if (reduce < nextImage.innerImg.width / nextImage.width) {
+    nextStep();
   } else {
     draw();
     requestId = window.requestAnimationFrame(step);
   }
-  // draw();
 };
 
 const draw = () => {
-  const currentImage = imagesData[0];
-
-  const nextImage = imagesData[1];
-
+  console.log("drawing");
   // 重复计算
   const temp1 =
-    (nextImage.width - nextImage.innerImg.width / factor) /
+    (nextImage.width - nextImage.innerImg.width / reduce) /
     (nextImage.width - nextImage.innerImg.width);
 
   // 由局部小窗口在整个屏幕，缩小到整个图片都在屏幕
@@ -87,8 +104,8 @@ const draw = () => {
     nextImage.img,
     nextImage.innerImg.left * temp1, // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
     nextImage.innerImg.top * temp1, // EN/IT= AE/AI=AQ/AR
-    nextImage.innerImg.width / factor, //   开始是innerWidth，慢慢变大到imageOriginalWith
-    nextImage.innerImg.height / factor,
+    nextImage.innerImg.width / reduce, //   开始是innerWidth，慢慢变大到imageOriginalWith
+    nextImage.innerImg.height / reduce,
     0,
     0,
     canvasWidth,
@@ -97,7 +114,7 @@ const draw = () => {
 
   // 由整个图片都在屏幕，缩小到下一个图片的小窗口
   const asau =
-    (currentImage.width - currentImage.width * factor) /
+    (currentImage.width - currentImage.width * reduce) /
     (currentImage.width -
       (nextImage.innerImg.width * currentImage.width) / nextImage.width); // AS/AU
   ctx.drawImage(
@@ -108,12 +125,19 @@ const draw = () => {
     currentImage.height,
     (nextImage.innerImg.left * asau * canvasWidth) / nextImage.width, // 这里需要换算成实际的canvas的大小
     (nextImage.innerImg.top * asau * canvasHeight) / nextImage.height,
-    canvasWidth * factor,
-    canvasHeight * factor
+    canvasWidth * reduce,
+    canvasHeight * reduce
   );
 
-  factor = factor * 0.993;
+  reduce = reduce * factor;
 };
+
+onMounted(async () => {
+  ctx = canvas.value.getContext("2d");
+  await loadImages();
+  // 画布和图片准备好了，开始画图
+  start();
+});
 </script>
 
 <style lang="scss">
