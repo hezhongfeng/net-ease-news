2dc5a48a563d041e4a2195c596693b2930112d4b
hezhongfeng <287645587@qq.com>
2023-02-28 08:55:52 +0800

    :sparkles: feat(custom): 优化调整参数

--- src/components/PictureInPicture/PictureInPicture.vue
+++ src/components/PictureInPicture/PictureInPicture.vue
@@ -8,7 +8,7 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-// import Picture from "../../assets/images/cover_v2.jpg";
+import Cover from "../../assets/images/cover_v2.jpg";
 import P1 from "../../assets/images/p1.jpg";
 import P2 from "../../assets/images/p2.jpg";
 
@@ -25,6 +25,11 @@ let ctx = null;
 // const imageOriginalHeight = 3015;
 
 const imageDatas = [
+  {
+    src: Cover,
+    width: "750",
+    height: "1206",
+  },
   {
     src: P1,
     width: "1875",
@@ -51,6 +56,7 @@ const imageDatas = [
 
 // let radio = 556 / 1875;
 let radio = 1;
+// let radio = 0.2965333;
 
 let timer = null;
 
@@ -113,36 +119,37 @@ const draw = () => {
 
   const nextImage = imageDatas[1];
 
-  // 重复计算
-  const temp1 =
-    (nextImage.width - nextImage.innerImg.width / radio) /
-    (nextImage.width - nextImage.innerImg.width);
+  // // 重复计算
+  // const temp1 =
+  //   (nextImage.width - nextImage.innerImg.width / radio) /
+  //   (nextImage.width - nextImage.innerImg.width);
 
-  // 由局部小窗口在整个屏幕，缩小到整个图片都在屏幕
-  ctx.drawImage(
-    nextImage.img,
-    nextImage.innerImg.left * temp1, // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
-    nextImage.innerImg.top * temp1, // EN/IT= AE/AI=AQ/AR
-    nextImage.innerImg.width / radio, //   开始是innerWidth，慢慢变大到imageOriginalWith
-    nextImage.innerImg.height / radio,
-    0,
-    0,
-    canvasWidth,
-    canvasHeight
-  );
+  // // 由局部小窗口在整个屏幕，缩小到整个图片都在屏幕
+  // ctx.drawImage(
+  //   nextImage.img,
+  //   nextImage.innerImg.left * temp1, // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
+  //   nextImage.innerImg.top * temp1, // EN/IT= AE/AI=AQ/AR
+  //   nextImage.innerImg.width / radio, //   开始是innerWidth，慢慢变大到imageOriginalWith
+  //   nextImage.innerImg.height / radio,
+  //   0,
+  //   0,
+  //   canvasWidth,
+  //   canvasHeight
+  // );
 
   // 由整个图片都在屏幕，缩小到下一个图片的小窗口
   const asau =
     (currentImage.width - currentImage.width * radio) /
-    (currentImage.width - nextImage.innerImg.width); // AS/AU
+    (currentImage.width -
+      (nextImage.innerImg.width * currentImage.width) / nextImage.width); // AS/AU
   ctx.drawImage(
     currentImage.img,
     0,
     0,
     currentImage.width,
     currentImage.height,
-    (nextImage.innerImg.left * asau * canvasWidth) / currentImage.width, // 这里需要换算成实际的canvas的大小
-    (nextImage.innerImg.top * asau * canvasHeight) / currentImage.height,
+    (nextImage.innerImg.left * asau * canvasWidth) / nextImage.width, // 这里需要换算成实际的canvas的大小
+    (nextImage.innerImg.top * asau * canvasHeight) / nextImage.height,
     canvasWidth * radio,
     canvasHeight * radio
   );
