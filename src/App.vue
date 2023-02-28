a0ad7661a2b590702760c88d7b4cb7be61be3bcd
hezhongfeng <287645587@qq.com>
2023-02-28 11:17:02 +0800

    :pencil2: docs(custom): 说明

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="app">
-    <PictureInPicture />
+    <PictureInPicture :images-data="imagesData" />
   </div>
 </template>
 
 <script setup>
 import { PictureInPicture } from "./components/PictureInPicture";
+
+import Cover from "./assets/images/cover_v2.jpg";
+import P1 from "./assets/images/p1.jpg";
+import P2 from "./assets/images/p2.jpg";
+
+const imagesData = [
+  {
+    src: Cover,
+    width: "750",
+    height: "1206",
+  },
+  {
+    src: P1,
+    width: "1875",
+    height: "3015",
+    innerImg: {
+      width: "152",
+      height: "244",
+      left: "370",
+      top: "1068",
+    },
+  },
+  {
+    src: P2,
+    width: "1875",
+    height: "3015",
+    innerImg: {
+      width: "556",
+      height: "894",
+      left: "1251",
+      top: "1050",
+    },
+  },
+];
 </script>
 
 <style lang="scss">
--- src/components/PictureInPicture/PictureInPicture.vue
+++ src/components/PictureInPicture/PictureInPicture.vue
@@ -8,72 +8,35 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import Cover from "../../assets/images/cover_v2.jpg";
-import P1 from "../../assets/images/p1.jpg";
-import P2 from "../../assets/images/p2.jpg";
+
+// eslint-disable-next-line vue/no-setup-props-destructure
+const { imagesData } = defineProps(["imagesData"]);
 
 const canvas = ref();
 
-// canvas的ctx
 let ctx = null;
 
-// 图片最初的尺寸
-// const imageOriginalWith = 750;
-// const imageOriginalHeight = 1206;
+// 最初的缩小系数
+let factor = 1;
 
-// const imageOriginalWith = 1875;
-// const imageOriginalHeight = 3015;
+// requestAnimationFrame的请求ID，可以用来暂停和取消
+let requestId = null;
 
-const imageDatas = [
-  {
-    src: Cover,
-    width: "750",
-    height: "1206",
-  },
-  {
-    src: P1,
-    width: "1875",
-    height: "3015",
-    innerImg: {
-      width: "152",
-      height: "244",
-      left: "370",
-      top: "1068",
-    },
-  },
-  {
-    src: P2,
-    width: "1875",
-    height: "3015",
-    innerImg: {
-      width: "556",
-      height: "894",
-      left: "1251",
-      top: "1050",
-    },
-  },
-];
-
-// let radio = 556 / 1875;
-let radio = 1;
-// let radio = 0.2965333;
-
-let timer = null;
-
+// 加载所有图片
 const loadImages = async () => {
   const promises = [];
-  for (const imageData of imageDatas) {
+  for (const item of imagesData) {
     promises.push(
       new Promise((resolve, reject) => {
         const img = new Image();
-        img.src = imageData.src;
+        img.src = item.src;
         img.onload = () => {
           resolve();
         };
         img.onerror = () => {
           reject();
         };
-        imageData.img = img;
+        item.img = img;
       })
     );
   }
@@ -83,63 +46,58 @@ const loadImages = async () => {
 };
 
 onMounted(async () => {
-  await loadImages();
-
   ctx = canvas.value.getContext("2d");
+  await loadImages();
+  // 画布和图片准备好了，开始画图
   start();
 });
-
-const start = () => {
-  console.log(ctx);
-
-  step();
-};
-
-const step = () => {
-  // 缩小后停止;
-  if (radio < 556 / 1875) {
-    console.log(72);
-    window.cancelAnimationFrame(timer);
-  } else {
-    draw();
-    timer = window.requestAnimationFrame(step);
-  }
-  // draw();
-};
 
 const canvasWidth = document.documentElement.clientWidth;
 
 // 按照图片的长宽比，计算出canvas的高度，注意不能使用样式去控制canvas
 const canvasHeight = canvasWidth / 0.6218;
 
+const start = () => {
+  step();
+};
+
+const step = () => {
+  // 缩小到PIP的大小和位置
+  if (factor < 152 / 1875) {
+    window.cancelAnimationFrame(requestId);
+  } else {
+    draw();
+    requestId = window.requestAnimationFrame(step);
+  }
+  // draw();
+};
+
 const draw = () => {
-  console.log("drawing");
+  const currentImage = imagesData[0];
 
-  const currentImage = imageDatas[0];
+  const nextImage = imagesData[1];
 
-  const nextImage = imageDatas[1];
+  // 重复计算
+  const temp1 =
+    (nextImage.width - nextImage.innerImg.width / factor) /
+    (nextImage.width - nextImage.innerImg.width);
 
-  // // 重复计算
-  // const temp1 =
-  //   (nextImage.width - nextImage.innerImg.width / radio) /
-  //   (nextImage.width - nextImage.innerImg.width);
-
-  // // 由局部小窗口在整个屏幕，缩小到整个图片都在屏幕
-  // ctx.drawImage(
-  //   nextImage.img,
-  //   nextImage.innerImg.left * temp1, // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
-  //   nextImage.innerImg.top * temp1, // EN/IT= AE/AI=AQ/AR
-  //   nextImage.innerImg.width / radio, //   开始是innerWidth，慢慢变大到imageOriginalWith
-  //   nextImage.innerImg.height / radio,
-  //   0,
-  //   0,
-  //   canvasWidth,
-  //   canvasHeight
-  // );
+  // 由局部小窗口在整个屏幕，缩小到整个图片都在屏幕
+  ctx.drawImage(
+    nextImage.img,
+    nextImage.innerImg.left * temp1, // 最初是innerLeft，最后是0 AN/AT = AE/AI=AQ/AR
+    nextImage.innerImg.top * temp1, // EN/IT= AE/AI=AQ/AR
+    nextImage.innerImg.width / factor, //   开始是innerWidth，慢慢变大到imageOriginalWith
+    nextImage.innerImg.height / factor,
+    0,
+    0,
+    canvasWidth,
+    canvasHeight
+  );
 
   // 由整个图片都在屏幕，缩小到下一个图片的小窗口
   const asau =
-    (currentImage.width - currentImage.width * radio) /
+    (currentImage.width - currentImage.width * factor) /
     (currentImage.width -
       (nextImage.innerImg.width * currentImage.width) / nextImage.width); // AS/AU
   ctx.drawImage(
@@ -150,11 +108,11 @@ const draw = () => {
     currentImage.height,
     (nextImage.innerImg.left * asau * canvasWidth) / nextImage.width, // 这里需要换算成实际的canvas的大小
     (nextImage.innerImg.top * asau * canvasHeight) / nextImage.height,
-    canvasWidth * radio,
-    canvasHeight * radio
+    canvasWidth * factor,
+    canvasHeight * factor
   );
 
-  radio = radio * 0.993;
+  factor = factor * 0.993;
 };
 </script>
 
